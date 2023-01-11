3086c48383bed7b362649b4a59e29513045878ac
Censuradho <gustavoleiteoliveira800@gmail.com>
2023-01-11 11:05:00 -0300

    feat(analytics)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 
 import type { AppProps } from 'next/app'
 import { useEffect } from 'react'
+import reactGA from "react-ga4";
 
 import { globalStyle } from 'stitches.config'
 
@@ -8,6 +9,10 @@ import { globalStyle } from 'stitches.config'
 export default function MyApp ({ Component, pageProps }: AppProps) {
   useEffect(() => {
     globalStyle()
+    
+    if (process.env.NODE_ENV === 'production') {
+      reactGA.initialize(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS as string);
+    }
   }, [])
 
   return (
