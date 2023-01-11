93da73c3705a7373c6e32b0cbd1579fa88350888
Censuradho <gustavoleiteoliveira800@gmail.com>
2023-01-11 12:08:25 -0300

    feat(favicon): add more sizes

--- components/common/head/index.tsx
+++ components/common/head/index.tsx
@@ -39,6 +39,7 @@ export function Head (props: HeadProps) {
       <meta name="msapplication-TileColor" content="#da532c" />
       <meta name="theme-color" content="#ffffff" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" href="/favicon.ico" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
     </NextHead>
