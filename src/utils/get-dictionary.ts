ad9628f47d0624a45283d84e0e6a9e5b2c5bc9d5
censuradho <gustavoleiteoliveira800@gmail.com>
2023-06-14 10:58:35 -0300

    feat(get dictionaries util)

---
+++ src/utils/get-dictionary.ts
@@ -0,0 +1,9 @@
+import 'server-only'
+import type { Locale } from '@/config/i18n'
+
+const dictionaries = {
+  'pt-BR': () => import('@/dictionaries/pt-BR.json').then((module) => module.default),
+  en: () => import('@/dictionaries/pt-BR.json').then((module) => module.default),
+}
+
+export const getDictionary = async (locale: Locale) => dictionaries[locale]()
