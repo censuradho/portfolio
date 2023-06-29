e097f5dd57caeb858384788c7ca9239d2d61eee3
Censuradho <gustavoleiteoliveira800@gmail.com>
2023-06-29 14:39:16 -0300

    feat(lib): add Ghost

--- src/config/app.ts
+++ src/config/app.ts
@@ -21,8 +21,8 @@ export const appSettings = {
     }
   },
   ghost: {
-    url: process.env.GHOST_URL,
-    key: process.env.GHOST_KEY,
+    url: process.env.GHOST_URL || '',
+    key: process.env.GHOST_KEY || '',
   }
 } 
 
