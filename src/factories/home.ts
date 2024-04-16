329b68f95fc8350e9b2d899aa91dc5e879717a0b
censuradho <gustavo.leite@vortigo.com.br>
2024-04-16 19:08:19 -0300

    feat(contentful): add image factory

---
+++ src/factories/home.ts
@@ -0,0 +1,20 @@
+function imageFactory (data: any) {
+  const { fields } = data || {}
+
+  return {
+    title: fields?.title || '',
+    description: fields?.description || '',
+    file: {
+      url: fields?.file?.url || '',
+      filename: fields?.file?.filename || '',
+      contentType: fields?.file?.contentType || '',
+      details: {
+        size: fields?.file?.details?.size || '',
+        image: {
+          width: fields?.file?.details?.image?.width || 0,
+          height: fields?.file?.details?.image?.height || 0,
+        },
+      }
+    }
+  }
+}
